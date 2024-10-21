7d3c4b5219d31a4635376fa20e5ffbe3a56aac21
Alex-320 <olekmytnyk@gmail.com>
2024-10-21 15:36:44 +0200

    Edit file global.header-nav.css and global.header-nav.partial.html

--- js/header.js
+++ js/header.js
@@ -1,19 +1,18 @@
-
 const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const btnSub = document.querySelector(".header__menu-btn");
-const menuLinks = document.querySelectorAll(".header__menu-link"); // 
+const menuLinks = document.querySelectorAll(".header__menu-link");
+
 
 btnSub.addEventListener("click", () => {
-    body.classList.toggle("overflow-hidden");
-    header.classList.toggle("nav-visible");
+    body.classList.toggle("overflow-hidden");  
+    header.classList.toggle("nav-visible");    
 });
 
 
 menuLinks.forEach(link => {
     link.addEventListener("click", () => {
-        body.classList.remove("overflow-hidden");
-        header.classList.remove("nav-visible");
+        body.classList.remove("overflow-hidden");  
+        header.classList.remove("nav-visible");    
     });
 });
-
