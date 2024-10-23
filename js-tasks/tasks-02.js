2f340be1b35b9445a931003749f91e400b7d731c
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2024-10-23 17:12:15 +0300

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,36 +1,35 @@
-1.    /////////////////https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-function stringToArray(string){
-return string.split(" ");
+1; /////////////////https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+function stringToArray(string) {
+  return string.split(" ");
 }
 
-
-2.    //////////////////https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+2; //////////////////https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 function DNAtoRNA(dna) {
-  return dna.split('').map(item=>item ==='T'?'U':item).join('')
+  return dna
+    .split("")
+    .map((item) => (item === "T" ? "U" : item))
+    .join("");
 }
-
 
 function DNAtoRNA(dna) {
-  return dna.replaceAll('T', 'U');
+  return dna.replaceAll("T", "U");
 }
 
+3; /////////////////https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+const min = function (list) {
+  return Math.min(...list);
+};
 
-3. /////////////////https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
-const min = function(list){
-    return Math.min(...list);
+const max = function (list) {
+  return Math.max(...list);
+};
+
+4; ////////////////https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+function min(arr, toReturn) {
+  return toReturn === "value"
+    ? Math.min(...arr)
+    : arr.indexOf(Math.min(...arr));
 }
-
-const max = function(list){
-    return Math.max(...list);
-}
-
-4. ////////////////https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
-function min(arr, toReturn) { 
-    return toReturn === "value" ? Math.min(...arr):arr.indexOf(Math.min(...arr));
-}
-
-
-
 
 ///////////////https://www.codewars.com/kata/53ee5429ba190077850011d4
 
@@ -39,14 +38,11 @@ function doubleInteger(i) {
   return i;
 }
 
-
 ////////////////https://www.codewars.com/kata/5545f109004975ea66000086
 
 function isDivisible(n, x, y) {
   return n % x === 0 && n % y === 0;
 }
-
-
 
 ////////////////https://www.codewars.com/kata/5b853229cfde412a470000d0
 
@@ -54,33 +50,26 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
   return Math.abs(dadYearsOld - 2 * sonYearsOld);
 }
 
-
-
 //////////////https://www.codewars.com/kata/574b3b1599d8f897470018f6
 
 function getRealFloor(n) {
-  if(n > 0 && n <= 13){
-    return n -1;
-  }else if(n > 13){
+  if (n > 0 && n <= 13) {
+    return n - 1;
+  } else if (n > 13) {
     return n - 2;
-  }
-  else{
+  } else {
     return n;
   }
 }
 
-
-
 ///////////https://www.codewars.com/kata/5933a1f8552bc2750a0000ed
 
-function nthEven(n){
+function nthEven(n) {
   return n * 2 - 2;
 }
 
-
-
 //////////////https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a
 
-function past(h, m, s){
-  return ((h * 3600) + (m * 60) + s) * 1000;
+function past(h, m, s) {
+  return (h * 3600 + m * 60 + s) * 1000;
 }
