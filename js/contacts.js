5a8723bee8fb1e9daef254924b1f657b950514f8
Anna Romanovska <sup2a1nn@gmail.com>
2024-10-26 18:53:48 +0300

    Placed featured books list into index.featured-books-list.js file

--- js/contacts.js
+++ js/contacts.js
@@ -1,5 +1,6 @@
 function init() {
     import('./header.js');
+    import('./contacts.form-validation.js');
   }
   
   const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 function init() {
     import("./header.js");
+    import("./index.featured-books-list.js");
   }
   
   const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
